4ec7dacb312bbd3884561891def6c6a7a6bb30b0
Martin Feuerhahn <sdk@c2call.com>
2013-08-23 17:40:27 +0500

    SDK Simple Dialer

---
+++ SDK-SimpleDialer Sample/SPAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SPAppDelegate.h
+//  SDK-SimpleDialer Sample
+//
+//  Created by Michael Knecht on 02.06.13.
+//  Copyright (c) 2013 C2Call GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SocialCommunication/SocialCommunication.h>
+
+@interface SPAppDelegate : C2CallAppDelegate
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SDK-SimpleDialer Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDK-SimpleDialer Sample
+//
+//  Created by Michael Knecht on 02.06.13.
+//  Copyright (c) 2013 C2Call GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
